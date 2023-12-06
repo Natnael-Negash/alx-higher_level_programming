8bfb4f0d5a0142fef4e64cc37ed62a4797a24e56
Natnael-Negash <nnegash658@gmail.com>
2023-12-06 10:54:06 -0800

    python more data structure

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    newsquare = []
+    a = 0
+
+    for x in matrix:
+        newsquare.append([])
+
+        for y in matrix[a]:
+            newsquare[a}.append(y**2)
