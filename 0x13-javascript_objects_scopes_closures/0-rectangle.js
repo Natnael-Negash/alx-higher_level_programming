cbd807029bd30d123854ad93b8975576dfd7aee4
Natnael-Negash <nnegash658@gmail.com>
2024-03-12 11:07:24 -0900

    js objets, scopes and closures

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// write an empty Rectangle class that define the rectangle
+module.exports = class Rectangle { };
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// define the rectangle class with width and height attribute
+modules.exports = class Rectangle {
+    constructor (width,height) {
+        this.width = width;
+        this.height = height;
+    }
+};
